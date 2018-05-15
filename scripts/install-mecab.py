9dc1a83470097fa2289c3a8d41901b3e004e1879
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-05-15 21:18:36 +0900

    Use wget instead of curl

--- scripts/install-mecab.py
+++ scripts/install-mecab.py
@@ -2,13 +2,15 @@ import os
 import subprocess
 import sys
 
+from urllib.request import urlopen
+
 MECAB_KO_URL = 'https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz'
 MECAB_KO_DIC_URL = 'https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.0.3-20170922.tar.gz'
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 
 print('Installing mecab-ko...')
-subprocess.check_call(['curl', '-O', '-L', MECAB_KO_URL])
+subprocess.check_call(['wget', '--progress=dot:binary', MECAB_KO_URL])
 subprocess.check_call(['tar', 'xvfz', 'mecab-0.996-ko-0.9.2.tar.gz'])
 
 cwd = os.path.join(base_path, 'mecab-0.996-ko-0.9.2')
@@ -21,7 +23,7 @@ subprocess.check_call(['make'], cwd=cwd)
 subprocess.check_call(['make', 'install'], cwd=cwd)
 
 print('Installing mecab-ko-dic...')
-subprocess.check_call(['curl', '-O', '-L', MECAB_KO_DIC_URL])
+subprocess.check_call(['wget', '--progress=dot:binary', MECAB_KO_DIC_URL])
 subprocess.check_call(['tar', 'xvfz', 'mecab-ko-dic-2.0.3-20170922.tar.gz'])
 
 cwd = os.path.join(base_path, 'mecab-ko-dic-2.0.3-20170922')
