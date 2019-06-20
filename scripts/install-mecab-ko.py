df2058ec5f6943c2b1405665a0a834d1f533eee9
Jonghwan Hyeon <hyeon0145@gmail.com>
2019-06-21 02:01:42 +0900

    Update mecab-ko-dic to 2.1.1

--- scripts/install-mecab-ko.py
+++ scripts/install-mecab-ko.py
@@ -7,7 +7,7 @@ from tempfile import TemporaryDirectory
 from urllib.parse import urlparse
 
 MECAB_KO_URL = 'https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz'
-MECAB_KO_DIC_URL = 'https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.0.3-20170922.tar.gz'
+MECAB_KO_DIC_URL = 'https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz'
 
 
 @contextmanager
