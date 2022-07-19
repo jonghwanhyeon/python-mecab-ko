6909bc7af8f250c547726d4d395506a65fe004b1
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-07-20 02:15:47 +0900

    Version 1.0.13

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ with open('README.md', 'r', encoding='utf-8') as input_file:
 
 setup(
     name='python-mecab-ko',
-    version='1.0.12',
+    version='1.0.13',
     url='https://github.com/jonghwanhyeon/python-mecab-ko',
     author='Jonghwan Hyeon',
     author_email='hyeon0145@gmail.com',
