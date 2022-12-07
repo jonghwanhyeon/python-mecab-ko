6632b106498d43cbf7c6a53fa5b50e700aec4db5
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2022-12-07 17:43:43 +0900

    Use shared library

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
-import platform
+import shutil
 import site
 import subprocess
 import sys
 from glob import glob
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Optional
 
-from pybind11.setup_helpers import Pybind11Extension
-from pybind11.setup_helpers import build_ext as _build_ext
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import find_packages, setup
 
 prefix_paths = [
@@ -37,72 +37,92 @@ def guess_prefix() -> Path:
     raise RuntimeError("All prefix candidates are not writable")
 
 
-with TemporaryDirectory() as working_directory:
+class Executable:
+    def __init__(self, command: str):
+        self._command = command
 
-    class build_ext(_build_ext):
-        def build_extensions(self):
-            self._build_mecab()
+    def __call__(self, *args) -> str:
+        return subprocess.check_output(
+            [self.executable, *args], encoding="utf-8"
+        ).strip()
 
-            super().build_extensions()
+    def exists(self) -> bool:
+        return self.executable is not None
 
-        def _build_mecab(self):
-            setup_path = Path(__file__).parent.absolute()
-            scripts_path = setup_path / "scripts"
-            build_script_path = scripts_path / "build-mecab-ko.py"
+    @property
+    def executable(self) -> Optional[str]:
+        paths = [str(path / "bin") for path in prefix_paths]
+        paths.append(os.environ["PATH"])
+        return shutil.which(self._command, path=os.pathsep.join(paths))
 
-            subprocess.check_call(
+
+class MeCabExtension(Pybind11Extension):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        mecab_config = Executable("mecab-config")
+        if not mecab_config.exists():
+            self._install_mecab()
+
+        self.include_dirs.append(mecab_config("--inc-dir"))
+        self.library_dirs.append(mecab_config("--libs-only-L"))
+        self.libraries.append("mecab")
+
+    def _install_mecab(self):
+        setup_path = Path(__file__).parent.absolute()
+        scripts_path = setup_path / "scripts"
+        build_script_path = scripts_path / "build-mecab-ko.py"
+
+        with TemporaryDirectory() as working_directory:
+            subprocess.run(
                 [
                     sys.executable,
                     str(build_script_path),
+                    "--install",
                     "--prefix",
                     str(guess_prefix()),
                 ],
                 cwd=working_directory,
+                check=True,
             )
 
-    with open("README.md", "r", encoding="utf-8") as input_file:
-        long_description = input_file.read()
 
-    setup(
-        name="python-mecab-ko",
-        version="1.2.3",
-        url="https://github.com/jonghwanhyeon/python-mecab-ko",
-        author="Jonghwan Hyeon",
-        author_email="hyeon0145@gmail.com",
-        description="A python binding for mecab-ko",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        license="BSD",
-        keywords="mecab mecab-ko",
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: BSD License",
-            "Natural Language :: Korean",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3 :: Only",
-            "Topic :: Text Processing",
-            "Topic :: Text Processing :: Linguistic",
-        ],
-        zip_safe=False,
-        python_requires=">=3.7",
-        packages=find_packages(),
-        install_requires=["mecab_ko_dic"],
-        package_data={"mecab": ["mecabrc"]},
-        data_files=[("scripts", ["scripts/build-mecab-ko.py"])],
-        cmdclass={"build_ext": build_ext},
-        ext_modules=[
-            Pybind11Extension(
-                name="_mecab",
-                sources=sorted(glob("mecab/pybind/**/*.cpp", recursive=True)),
-                include_dirs=[
-                    "mecab/pybind/_mecab",
-                    os.path.join(working_directory, "src"),
-                ],
-                libraries=["iconv"] if platform.system() == "Darwin" else [],
-                extra_objects=[
-                    os.path.join(working_directory, "src", ".libs", "libmecab.a")
-                ],
-            ),
-        ],
-    )
+with open("README.md", "r", encoding="utf-8") as input_file:
+    long_description = input_file.read()
+
+setup(
+    name="python-mecab-ko",
+    version="1.2.3",
+    url="https://github.com/jonghwanhyeon/python-mecab-ko",
+    author="Jonghwan Hyeon",
+    author_email="hyeon0145@gmail.com",
+    description="A python binding for mecab-ko",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    keywords="mecab mecab-ko",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: Korean",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    zip_safe=False,
+    python_requires=">=3.7",
+    packages=find_packages(),
+    install_requires=["mecab_ko_dic"],
+    package_data={"mecab": ["mecabrc"]},
+    data_files=[("scripts", ["scripts/build-mecab-ko.py"])],
+    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        MeCabExtension(
+            name="_mecab",
+            sources=sorted(glob("mecab/pybind/**/*.cpp", recursive=True)),
+            include_dirs=["mecab/pybind/_mecab"],
+        ),
+    ],
+)
