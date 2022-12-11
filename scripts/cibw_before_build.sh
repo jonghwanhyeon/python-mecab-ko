b41f33c7747de85bac6ab478b40b3ae8c4657b46
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2022-12-12 00:16:00 +0900

    Update CI/CD

--- scripts/cibw_before_build.sh
+++ scripts/cibw_before_build.sh
@@ -2,13 +2,35 @@
 
 set -ex
 
-PLATFORM="$(uname -s)"
-
 PROJECT="$1"
-INSTALL_MECAB_KO=${PROJECT}/scripts/install_mecab_ko.py
+PLATFORM=$(python -c "import platform; print(platform.system())")
 
-if [[ $PLATFORM == "Linux" ]] ; then
-    python ${INSTALL_MECAB_KO}
+prepare_linux() {
+    python ${PROJECT}/scripts/install_mecab_ko.py
+}
+
+prepare_darwin() {
+    sudo python ${PROJECT}/scripts/install_mecab_ko.py
+}
+
+prepare_windows() {
+    if [[ $CIBW_ARCHS == "x86" ]]; then
+        architecture="x86"
+    elif [[ $CIBW_ARCHS == "AMD64" ]]; then
+        architecture="x64"
+    fi
+
+    curl \
+        --location \
+        --output "libmecab-ko.zip" \
+        "https://github.com/jonghwanhyeon/mecab-ko/releases/download/0.996-ko-0.9.2/libmecab-0.996-ko-0.9.2-windows-${architecture}.zip"
+    unzip -o "libmecab-ko.zip" -d "${MECAB_PATH}"
+}
+
+if [[ $PLATFORM == "Linux" ]]; then
+    prepare_linux
 elif [[ $PLATFORM == "Darwin" ]]; then
-    sudo python ${INSTALL_MECAB_KO}
+    prepare_darwin
+elif [[ $PLATFORM == "Windows" ]]; then
+    prepare_windows
 fi
