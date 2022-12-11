0b406df17a114b6914b0951c8069f7775a3a1e40
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2022-12-11 15:02:29 +0900

    Sort imports

--- mecab/mecab.py
+++ mecab/mecab.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 from typing import List, NamedTuple, Optional, Tuple, Union
-import os
+
 import mecab_ko_dic
 
 import _mecab
