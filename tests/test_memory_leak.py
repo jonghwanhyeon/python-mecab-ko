156b6ad110ef2f8adfc70cd4cb2ee0c4606efd70
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-12-26 16:19:56 +0900

    Add test for detecting memory leak

---
+++ tests/test_memory_leak.py
@@ -0,0 +1,66 @@
+import gc
+import os
+from contextlib import contextmanager
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+import psutil
+
+from mecab import MeCab, Morpheme
+
+tests_path = Path(__file__).parent.absolute()
+corpus_path = tests_path / "corpus.txt"
+
+
+@dataclass
+class MemoryUsage:
+    start: int = 0
+    end: int = 0
+
+    @property
+    def leaked(self) -> int:
+        return self.end - self.start
+
+
+@contextmanager
+def trace_memory_usage():
+    process = psutil.Process(os.getpid())
+    memory_usage = MemoryUsage()
+
+    gc.collect()
+    memory_usage.start = process.memory_info().rss
+
+    yield memory_usage
+
+    gc.collect()
+    memory_usage.end = process.memory_info().rss
+
+
+def readlines(path: Path) -> List[str]:
+    lines = path.read_text(encoding="utf-8").strip()
+    return lines.splitlines()
+
+
+def parse_corpus(corpus_path: Path) -> List[Morpheme]:
+    mecab = MeCab()  # Create a MeCab instance each time.
+
+    morphemes = []
+    for line in readlines(corpus_path):
+        morphemes.extend(mecab.parse(line))
+    return morphemes
+
+
+def test_memory_leak():
+    number_of_tries = 10
+
+    list_of_number_of_morphemes = [0] * number_of_tries
+
+    with trace_memory_usage() as total_memory_usage:
+        for index in range(number_of_tries):
+            with trace_memory_usage() as memory_usage:
+                list_of_number_of_morphemes[index] = len(parse_corpus(corpus_path))
+            assert memory_usage.leaked < (5 * 1024 * 1024)
+    assert total_memory_usage.leaked < (5 * 1024 * 1024)
+
+    assert len(set(list_of_number_of_morphemes)) == 1
