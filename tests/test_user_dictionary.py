af6a1af8e8d6252b45acebceb0ff1204b9d29874
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-12-27 22:15:32 +0900

    Refactor test case

--- tests/test_user_dictionary.py
+++ tests/test_user_dictionary.py
@@ -9,7 +9,7 @@ import pytest
 from mecab import Feature, MeCab
 
 
-class Word(NamedTuple):
+class Morpheme(NamedTuple):
     surface: str
     feature: Feature
     # Lower cost has higher priority
@@ -29,25 +29,14 @@ class Word(NamedTuple):
         )
 
 
-def number_of_dictionaries(mecab: MeCab):
-    count = 0
-
-    dictionary_info = mecab._tagger.dictionary_info()
-    while dictionary_info is not None:
-        count += 1
-        dictionary_info = dictionary_info.next
-
-    return count
-
-
-def build_user_dictionary(words: List[Word], output_path: Path) -> Path:
+def build_user_dictionary(morphemes: List[Morpheme], output_path: Path) -> Path:
     csv_path = output_path / f"{uuid.uuid4()}.csv"
     dictionary_path = csv_path.with_suffix(".dic")
 
     # First, create user dictionary from words as CSV format
     with open(csv_path, "w", encoding="utf-8") as output_file:
-        for word in words:
-            print(str(word), file=output_file)
+        for morpheme in morphemes:
+            print(str(morpheme), file=output_file)
 
     # Then, build user dictionary to output_path
     subprocess.run(
@@ -70,7 +59,7 @@ def build_user_dictionary(words: List[Word], output_path: Path) -> Path:
 def twitch_user_dictionary_path(tmp_path: Path):
     return build_user_dictionary(
         [
-            Word("트위치", Feature(pos="NNP", has_jongseong=False)),
+            Morpheme("트위치", Feature(pos="NNP", has_jongseong=False)),
         ],
         tmp_path,
     )
@@ -80,14 +69,14 @@ def twitch_user_dictionary_path(tmp_path: Path):
 def platform_user_dictionary_path(tmp_path: Path):
     return build_user_dictionary(
         [
-            Word("플랫폼", Feature(pos="NNG", has_jongseong=True)),
+            Morpheme("플랫폼", Feature(pos="NNG", has_jongseong=True)),
         ],
         tmp_path,
     )
 
 
 def test_single_user_dictionary(mecab: MeCab, twitch_user_dictionary_path: Path):
-    assert number_of_dictionaries(mecab) == 1
+    assert len(mecab.dictionary) == 1
     assert mecab.pos("트위치는 양방향 생방송 플랫폼입니다") == [
         ("트", "NNG"),
         ("위치", "NNG"),
@@ -100,7 +89,7 @@ def test_single_user_dictionary(mecab: MeCab, twitch_user_dictionary_path: Path)
     ]
 
     mecab_with_user_dictionary = MeCab(user_dictionary_path=twitch_user_dictionary_path)
-    assert number_of_dictionaries(mecab_with_user_dictionary) == 2
+    assert len(mecab_with_user_dictionary.dictionary) == 2
     assert mecab_with_user_dictionary.pos("트위치는 양방향 생방송 플랫폼입니다") == [
         ("트위치", "NNP"),
         ("는", "JX"),
@@ -113,7 +102,7 @@ def test_single_user_dictionary(mecab: MeCab, twitch_user_dictionary_path: Path)
 
 
 def test_multiple_user_dictionary(mecab: MeCab, twitch_user_dictionary_path: Path, platform_user_dictionary_path: Path):
-    assert number_of_dictionaries(mecab) == 1
+    assert len(mecab.dictionary) == 1
     assert mecab.pos("트위치는 양방향 생방송 플랫폼입니다") == [
         ("트", "NNG"),
         ("위치", "NNG"),
@@ -128,7 +117,7 @@ def test_multiple_user_dictionary(mecab: MeCab, twitch_user_dictionary_path: Pat
     mecab_with_user_dictionary = MeCab(
         user_dictionary_path=[twitch_user_dictionary_path, platform_user_dictionary_path]
     )
-    assert number_of_dictionaries(mecab_with_user_dictionary) == 3
+    assert len(mecab_with_user_dictionary.dictionary) == 3
     assert mecab_with_user_dictionary.pos("트위치는 양방향 생방송 플랫폼입니다") == [
         ("트위치", "NNP"),
         ("는", "JX"),
