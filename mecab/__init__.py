0f6325731354af6b7e776df7937903a02c1d4af3
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2024-04-10 22:53:15 +0900

    Version 1.3.4

--- mecab/__init__.py
+++ mecab/__init__.py
@@ -1,7 +1,7 @@
 from .mecab import MeCab, MeCabError, mecabrc_path
 from .types import Dictionary, Feature, Morpheme, Span
 
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 
 __all__ = [
     "MeCab",
