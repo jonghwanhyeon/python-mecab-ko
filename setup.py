371e82cf35c3f6aa3d68549240e335e4e7bb46a7
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2024-07-14 17:31:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from glob import glob
 from pathlib import Path
 from typing import Optional
 
-from pybind11.setup_helpers import Pybind11Extension
-from pybind11.setup_helpers import build_ext as _build_ext
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 is_windows = platform.system() == "Windows"
@@ -26,6 +25,9 @@ class Executable:
         self._command = command
 
     def __call__(self, *args) -> str:
+        if self.executable is None:
+            raise RuntimeError(f"{self._command} not found")
+
         return subprocess.check_output([self.executable, *args], encoding="utf-8").strip()
 
     def exists(self) -> bool:
@@ -38,12 +40,10 @@ class Executable:
         return shutil.which(self._command, path=os.pathsep.join(paths))
 
 
-class unix_build_ext(_build_ext):
+class unix_build_ext(build_ext):
     def build_extension(self, extension):
         if extension.name == "_mecab":
             mecab_config = Executable("mecab-config")
-            if not mecab_config.exists():
-                raise RuntimeError("mecab-ko not found")
 
             extension.include_dirs.append(mecab_config("--inc-dir"))
             extension.library_dirs.append(mecab_config("--libs-only-L"))
@@ -52,7 +52,7 @@ class unix_build_ext(_build_ext):
         super().build_extension(extension)
 
 
-class windows_ext(_build_ext):
+class windows_ext(build_ext):
     def build_extension(self, extension):
         if extension.name == "_mecab":
             extension.include_dirs.append(r"C:\mecab")
